ee9cb02a95bba90a5e926aed06c0a630db9fea0c
arthur740212 <arthur740212@gmail.com>
2022-01-29 12:21:07 -0800

    Static/Moving Platform Scripts and Prefab complete

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -3,18 +3,36 @@ using UnityEngine;
 public class MovingPlatform : Platform
 {
 
-  public bool isMoving;
-  public float maxDisplacementX;
+    public bool isMoving = true;
+    public float maxDisplacementX = 3.0f;
+    public float loopTime = 6.0f;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    currentPosition = this.GetComponent<Transform>().position;
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
+    [SerializeField] private float currentLoopTime = 0.0f;
+    [SerializeField] private Vector3 leftMost;
+    [SerializeField] private Vector3 rightMost;
 
-  }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = this.GetComponent<Transform>().position;
+        leftMost = currentPosition - new Vector3(maxDisplacementX, 0, 0);
+        rightMost = currentPosition + new Vector3(maxDisplacementX, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dt = Time.deltaTime;
+
+        if (isMoving) { MovementLoop(dt); }
+    }
+
+    void MovementLoop(float dt)
+    {
+        float halfLoopTime = loopTime/2.0f;
+        currentLoopTime = (currentLoopTime + dt) % loopTime;
+        this.GetComponent<Transform>().position = Vector3.Lerp(leftMost, rightMost, Mathf.Abs(currentLoopTime - halfLoopTime) / halfLoopTime);
+    }
+
 }
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
 
-  internal Vector3 currentPosition;
+    [SerializeField]
+    internal Vector3 currentPosition;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    currentPosition = this.GetComponent<Transform>().position;
-  }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = this.GetComponent<Transform>().position;
+    }
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Update is called once per frame
+    void Update()
+    {
 
-  }
+    }
 }
